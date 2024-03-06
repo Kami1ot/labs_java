9c1a19666575def0e8346ffad382c14d6da36176
teenee0 <qwerty.gogi@mail.ru>
2024-03-06 10:36:02 +0300

    1,2,3,4 lab_ver_4

--- src/tetxt.java
+++ src/tetxt.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class tetxt {
+    public static int a;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число:");
